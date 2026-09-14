1ce5f509e2bddcbfcc52ec6be2a3d3c8d79caafb
baxtershelly75-art <baxtershelly75@gmail.com>
2026-09-14 08:04:01 -0400

    Add chunked recipe test loader

---
+++ tests/load-recipes.mjs
@@ -0,0 +1,9 @@
+import fs from 'node:fs';
+
+export const RECIPE_FILES = Array.from({ length: 27 }, (_, i) =>
+  new URL(`../data/recipes-${String(i + 1).padStart(2, '0')}.json`, import.meta.url)
+);
+
+export function loadRecipes() {
+  return RECIPE_FILES.flatMap(url => JSON.parse(fs.readFileSync(url, 'utf8')));
+}
