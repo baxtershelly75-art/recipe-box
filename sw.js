31058651ea9cffdaff7cc6374f389c08cba76f0c
baxtershelly75-art <baxtershelly75@gmail.com>
2026-09-15 13:02:15 -0400

    Cache recipe chunks 50-51

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'recipe-box-v1-12';
-const RECIPE_ASSETS = Array.from({length:49},(_,i)=>`./data/recipes-${String(i+1).padStart(2,'0')}.json`);
+const CACHE = 'recipe-box-v1-13';
+const RECIPE_ASSETS = Array.from({length:51},(_,i)=>`./data/recipes-${String(i+1).padStart(2,'0')}.json`);
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest',...RECIPE_ASSETS];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
