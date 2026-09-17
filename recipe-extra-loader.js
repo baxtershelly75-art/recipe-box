7ed2d35da8844d52dda3c091fd7cfe53828fbc4d
baxtershelly75-art <baxtershelly75@gmail.com>
2026-09-17 19:16:34 -0400

    Load colorful one-pot dinner collection

--- recipe-extra-loader.js
+++ recipe-extra-loader.js
@@ -1,7 +1,7 @@
 // Load recipe additions kept outside the original 75 launch chunks.
 (async function loadExtraRecipes() {
   try {
-    const files = ['./data/recipes-76.json', './data/recipes-77.json', './data/recipes-78.json', './data/recipes-79.json', './data/recipes-80.json'];
+    const files = ['./data/recipes-76.json', './data/recipes-77.json', './data/recipes-78.json', './data/recipes-79.json', './data/recipes-80.json', './data/recipes-81.json'];
     const responses = await Promise.all(files.map(path => fetch(path)));
     for (let i = 0; i < responses.length; i += 1) {
       if (!responses[i].ok) throw new Error(`Could not load ${files[i]}`);
