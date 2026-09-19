d6005b3ecf2ac485cfa18405e2e67e4e5aa97ab3
baxtershelly75-art <baxtershelly75@gmail.com>
2026-09-18 20:07:56 -0400

    Precache recipe batch 90

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'recipe-box-v1-54';
+const CACHE = 'recipe-box-v1-55';
 const RECIPE_ASSETS = Array.from({length:75},(_,i)=>`./data/recipes-${String(i+1).padStart(2,'0')}.json`);
-const EXTRA_RECIPE_ASSETS = Array.from({length:14},(_,i)=>`./data/recipes-${i+76}.json`);
+const EXTRA_RECIPE_ASSETS = Array.from({length:15},(_,i)=>`./data/recipes-${i+76}.json`);
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./kitchen-fractions.js','./recipe-extra-loader.js','./recipe-helpers.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png',...RECIPE_ASSETS,...EXTRA_RECIPE_ASSETS];
 
 self.addEventListener('install', event => {
