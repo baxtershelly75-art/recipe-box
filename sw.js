ded07e1245dcfb2276a816813824850238cfaa41
baxtershelly75-art <baxtershelly75@gmail.com>
2026-09-20 21:24:11 -0400

    Refresh Recipe Box service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'recipe-box-v1-60';
+const CACHE = 'recipe-box-v1-61';
 const RECIPE_ASSETS = Array.from({length:75},(_,i)=>`./data/recipes-${String(i+1).padStart(2,'0')}.json`);
 const EXTRA_RECIPE_ASSETS = Array.from({length:18},(_,i)=>`./data/recipes-${i+76}.json`);
 const ASSETS = ['./','./index.html','./styles.css','./grocery-helpers.js','./app.js','./kitchen-fractions.js','./recipe-extra-loader.js','./recipe-helpers.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png',...RECIPE_ASSETS,...EXTRA_RECIPE_ASSETS];
